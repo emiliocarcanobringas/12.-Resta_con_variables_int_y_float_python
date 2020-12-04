58e4f4c0948c7e72351650f9f3cdcf0c4b88d006
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 20:09:42 -0600

    Add files via upload

---
+++ 12.-Resta_con_variables_int_y_float_python.py
@@ -0,0 +1,18 @@
+# Este programa muestra una resta con variables de tipo int y tipo float
+
+print("Este programa muestra una resta con variables de tipo int y tipo float")
+print("También muestra el tipo de variable que realiza la operación")
+numero1 = 7
+print("El primer valor es: ")
+print(numero1)
+numero2 = 3.1416
+print("El segundo valor es: ")
+print(numero2)
+restadeambos = numero1 - numero2
+print("El resultado de la resta es: ")
+print(restadeambos)
+print("El tipo de variable es: ")
+print(type(restadeambos))
+print("este programa fue escrito por Emilio Carcaño Bringas")
+
+# Este programa fue escrito por Emilio Carcaño Bringas
